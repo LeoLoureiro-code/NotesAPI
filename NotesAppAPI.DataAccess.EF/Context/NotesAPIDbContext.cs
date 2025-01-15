62a25646c5435817419d8c00afa7d90dcd56c3e1
Leonardo Loureiro <83887814+LeoLoureiro-code@users.noreply.github.com>
2025-01-14 19:11:13 -0700

    Update NotesAPIDbContext.cs

--- NotesAppAPI.DataAccess.EF/Context/NotesAPIDbContext.cs
+++ NotesAppAPI.DataAccess.EF/Context/NotesAPIDbContext.cs
@@ -27,7 +27,7 @@ public partial class NotesAPIDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=50.6.160.84;database=arkedste_Notes;, ServerVersion.Parse("5.7.23-mysql"));
+        => optionsBuilder.UseMySql(" ServerVersion.Parse("5.7.23-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
