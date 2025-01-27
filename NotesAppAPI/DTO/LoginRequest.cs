910b460aeb9c70981ae59a3cc240a766180279db
Leo Loureiro <leolou.90@gmail.com>
2025-01-26 19:36:02 -0700

    Add Data Transfer Object file

---
+++ NotesAppAPI/DTO/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace NotesAppAPI.DTO
+{
+    public class LoginRequest
+    {
+        public string UserEmail { get; set; } = null!;
+        public string UserPassword { get; set; } = null!;
+    }
+}
