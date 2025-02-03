a10d4453d182ca5c9c99d84f4203e3063eeb0328
Leo Loureiro <leolou.90@gmail.com>
2025-02-02 19:26:47 -0700

    Update user Model

--- NotesAppAPI.DataAccess.EF/Models/User.cs
+++ NotesAppAPI.DataAccess.EF/Models/User.cs
@@ -5,25 +5,22 @@ namespace NotesAppAPI.DataAccess.EF.Models;
 
 public class User
 {
+    public User() { }
 
-    public User()
-    {
-
-    }
-
-    public User( string userEmail, string userPassword)
+    public User(string userEmail, string userPassword)
     {
         UserEmail = userEmail;
         UserPassword = userPassword;
     }
 
     public int UserId { get; set; }
-
     public string UserEmail { get; set; } = null!;
-
     public string UserPassword { get; set; } = null!;
 
-    public virtual ICollection<Note> Notes { get; set; } = new List<Note>();
+    // ✅ Added Refresh Token properties
+    public string? RefreshToken { get; set; }
+    public DateTime? RefreshTokenExpiryTime { get; set; }
 
+    public virtual ICollection<Note> Notes { get; set; } = new List<Note>();
     public virtual ICollection<NotesTag> NotesTags { get; set; } = new List<NotesTag>();
 }
