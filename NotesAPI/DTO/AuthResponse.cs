792004f392a41fa7f22fbe7d643bbcda37cf19a2
Leo <leolou.90@gmail.com>
2026-06-11 18:45:06 -0700

    Add DTO's

---
+++ NotesAPI/DTO/AuthResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace NotesAPI.DTO
+{
+    public class AuthResponse
+    {
+        public string Token { get; set; } = string.Empty;
+    }
+}
---
+++ NotesAPI/DTO/RegisterRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NotesAPI.DTO
+{
+    public class RegisterRequest
+    {
+        [Required]
+        string Email { get; set; } = string.Empty;
+        [Required]
+        string Password { get; set; } = string.Empty;
+
+    }
+}
