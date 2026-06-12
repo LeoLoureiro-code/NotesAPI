33e74593a1bd645e692b7964347fb7310d93ac94
Leo <leolou.90@gmail.com>
2026-06-11 17:54:18 -0700

    Add configurations to context

---
+++ NotesAPI.EF.Data/Context/NotesAPIDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotesAPI.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesAPI.EF.Data.Context
+{
+    public class NotesAPIDbContext: DbContext
+    {
+        public NotesAPIDbContext(DbContextOptions<NotesAPIDbContext> options)
+       : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<Note> Notes { get; set; } = null!;
+        public DbSet<Tag> Tags { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(
+                typeof(NotesAPIDbContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
