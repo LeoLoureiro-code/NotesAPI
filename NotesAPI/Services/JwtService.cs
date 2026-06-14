5dbb4e1fd3417175041e04a1d00454af79939011
Leo <leolou.90@gmail.com>
2026-06-13 20:15:26 -0700

    Change name of services

---
+++ NotesAPI/Services/JwtService.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using NotesAPI.Core.Entities;
+using NotesAPI.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesAPI.EF.Data.Services
+{
+    public class JwtService : IJwtService
+    {
+
+        private readonly IConfiguration _configuration;
+
+        public JwtService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string GenerateToken(User user)
+        {
+            ArgumentNullException.ThrowIfNull(user);
+
+            var jwtSettings = _configuration.GetSection("Jwt");
+
+            var key = jwtSettings["Key"]!;
+            var issuer = jwtSettings["Issuer"]!;
+            var audience = jwtSettings["Audience"]!;
+            var expires = DateTime.UtcNow.AddMinutes(int.Parse(jwtSettings["ExpirationInMinutes"]!));
+
+            var claims = new[]
+            {
+                new Claim(
+                    ClaimTypes.NameIdentifier, user.UserId.ToString()),
+
+                new Claim(ClaimTypes.Email, user.Email),
+
+                new Claim(ClaimTypes.Role, user.Role.ToString())
+            };
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: issuer,
+                audience: audience,
+                claims: claims,
+                expires: expires,
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ NotesAPI/Services/PasswordHasherService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using NotesAPI.Core.Entities;
+using NotesAPI.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesAPI.EF.Data.Services
+{
+    public class PasswordHasherService: IPasswordHasher
+    {
+
+        private readonly PasswordHasher<User> _passwordHasher = new();
+        public string HashPassword(string password)
+        {
+            return _passwordHasher.HashPassword(new User(), password);
+        }
+
+        public bool VerifyPassword(string hashedPassword, string providedPassword)
+        {
+            var result = _passwordHasher.VerifyHashedPassword(
+            new User(),
+            hashedPassword,
+            providedPassword);
+
+            return result == PasswordVerificationResult.Success;
+        }
+    }
+}
